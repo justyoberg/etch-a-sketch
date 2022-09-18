0090c2f037de583d77c98f0c54f788c914999084
Justy Oberg <justyoberg1@gmail.com>
2022-09-18 12:27:45 -0700

    Fix grid size implementation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 const canvas = document.getElementById("canvas");
-var grid_size = 1;
+const submit = document.querySelector(".submit");
+var gridSize = 16;
 
+submit.addEventListener("click", function() {
+  gridSize = document.querySelector(".grid-size").value;
+});
 
-const smalldiv = document.createElement("div");
-smalldiv.style.width = canvas.offsetWidth / grid_size + "px";
-smalldiv.style.height = canvas.offsetHeight / grid_size + "px";
-smalldiv.style.border = "thick solid #0000FF";
-
-
-canvas.appendChild(smalldiv);
+for (let i = 0; i < gridSize * gridSize; i++){
+  const smalldiv = document.createElement("div");
+  smalldiv.style.width = (canvas.offsetWidth / gridSize) + "px";
+  smalldiv.style.height = (canvas.offsetHeight / gridSize) + "px";
+  smalldiv.style.margin = "0";
+  smalldiv.style.padding = "0";
+  canvas.appendChild(smalldiv);
+}
