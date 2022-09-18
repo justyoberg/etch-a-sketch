be1b862cb42c9db9697b21d79235b68c0747cd57
Justy Oberg <justyoberg1@gmail.com>
2022-09-18 13:05:59 -0700

    Style the page

--- scripts.js
+++ scripts.js
@@ -10,7 +10,18 @@ for (let i = 0; i < gridSize * gridSize; i++){
   const smalldiv = document.createElement("div");
   smalldiv.style.width = (canvas.offsetWidth / gridSize) + "px";
   smalldiv.style.height = (canvas.offsetHeight / gridSize) + "px";
-  smalldiv.style.margin = "0";
-  smalldiv.style.padding = "0";
+  smalldiv.classList.add("grid-square");
   canvas.appendChild(smalldiv);
 }
+
+const gridSquares = document.querySelectorAll(".grid-square");
+
+function events(square) {
+  square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "black";
+  });
+  square.addEventListener("mousemove", function() {
+    square.style.backgroundColor = "black";
+  });
+}
+gridSquares.forEach(square => events(square));
