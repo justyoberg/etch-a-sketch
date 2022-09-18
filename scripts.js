7049c064c5babba52ca0fe068ece724dc902fdd3
Justy Oberg <justyoberg1@gmail.com>
2022-09-18 13:37:48 -0700

    Finish grid size functionality

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
 const canvas = document.getElementById("canvas");
 const submit = document.querySelector(".submit");
+const currentSize = document.querySelector(".size");
+
 var gridSize = 16;
 
 submit.addEventListener("click", function() {
   gridSize = document.querySelector(".grid-size").value;
+  if (gridSize > 16) return;
+  clearGrid();
+  createGrid();
+  currentSize.textContent = `Current size: ${gridSize} x ${gridSize}`
 });
 
-for (let i = 0; i < gridSize * gridSize; i++){
-  const smalldiv = document.createElement("div");
-  smalldiv.style.width = (canvas.offsetWidth / gridSize) + "px";
-  smalldiv.style.height = (canvas.offsetHeight / gridSize) + "px";
-  smalldiv.classList.add("grid-square");
-  canvas.appendChild(smalldiv);
+function clearGrid() {
+  const squares = canvas.querySelectorAll(".grid-square");
+  squares.forEach((square) => square.parentNode.removeChild(square));
 }
 
-const gridSquares = document.querySelectorAll(".grid-square");
+function createGrid() {
+  for (let i = 0; i < gridSize * gridSize; i++){
+    const smalldiv = document.createElement("div");
+    smalldiv.style.width = (canvas.offsetWidth / gridSize) + "px";
+    smalldiv.style.height = (canvas.offsetHeight / gridSize) + "px";
+    smalldiv.classList.add("grid-square");
+    canvas.appendChild(smalldiv);
+  }
+  const gridSquares = document.querySelectorAll(".grid-square");
+  gridSquares.forEach(square => events(square));
+}
 
 function events(square) {
   square.addEventListener("mousedown", function() {
     square.style.backgroundColor = "black";
   });
-  square.addEventListener("mousemove", function() {
-    square.style.backgroundColor = "black";
-  });
 }
-gridSquares.forEach(square => events(square));
+
+createGrid();
